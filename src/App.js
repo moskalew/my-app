e306533dc502459a010bad1244321cdbe78bb9a5
andrei moskalev <moskalev.web.dev@gmail.com>
2024-10-05 08:12:55 +0300

    npm JSON server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
-function App() {
+export const App = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch('http://localhost:3005/products')
+      .then((loadedData) => loadedData.json())
+      .then((loadedProducts) => {
+        setProducts(loadedProducts);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      {isLoading ? (
+        <div className={styles.loader}></div>
+      ) : (
+        products.map(({ id, name, price }) => (
+          <div key={id}>
+            {name} - {price} руб
+          </div>
+        ))
+      )}
     </div>
   );
-}
-
-export default App;
+};
