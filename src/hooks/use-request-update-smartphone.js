867a6d50352dafd81c9088f5bdcc4f30c35df215
andrei moskalev <moskalev.web.dev@gmail.com>
2024-10-10 20:59:55 +0300

    Firebase. Обновление элементов в базе данных

--- src/hooks/use-request-update-smartphone.js
+++ src/hooks/use-request-update-smartphone.js
@@ -1,22 +1,21 @@
 import { useState } from 'react';
+import { ref, set } from 'firebase/database';
+import { db } from '../firebase';
 
 export const useRequestUpdateSmartphone = (refreshProducts) => {
   const [isUpdating, setIsUpdating] = useState(false);
 
   const requestUpdateSmartphone = () => {
     setIsUpdating(true);
-    fetch('http://localhost:3005/products/002', {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json;charset=utf-8' },
-      body: JSON.stringify({
-        name: 'Смартфон',
-        price: 17900,
-      }),
+
+    const smartphoneDbRef = ref(db, 'products/002');
+
+    set(smartphoneDbRef, {
+      name: 'Смартфон',
+      price: 16700,
     })
-      .then((rawResponse) => rawResponse.json())
       .then((response) => {
         console.log('Смартфон обновлён, ответ сервера:', response);
-        refreshProducts();
       })
       .finally(() => setIsUpdating(false));
   };
